f7ff95fd0419cd26fb251ab9b0a085a3e0c991ed
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2026-02-27 14:26:07 +0100

    refactor(vtkjs): Simplify hex color conversion in AnnotatedRhombicuboctahedronActor for improved readability

--- packages/tools/src/utilities/vtkjs/AnnotatedRhombicuboctahedronActor/index.js
+++ packages/tools/src/utilities/vtkjs/AnnotatedRhombicuboctahedronActor/index.js
@@ -167,11 +167,7 @@ function vtkAnnotatedRhombicuboctahedronActor(publicAPI, model) {
     const hexToRgb = (hex) => {
       const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
       return result
-        ? [
-            parseInt(result[1], 16),
-            parseInt(result[2], 16),
-            parseInt(result[3], 16),
-          ]
+        ? result.slice(1, 4).map((n) => parseInt(n, 16))
         : [255, 255, 255];
     };
 
